2d92d700b92c8decbd641033fbf1f042197c8f2d
Aradhya Agrawal <aradhya.gzb08@gmail.com>
2026-02-25 01:42:31 +0530

    Now the functions similar to set and get for each deployed contract have been added

--- StorageFactory.sol
+++ StorageFactory.sol
@@ -4,11 +4,15 @@ pragma solidity ^0.8.0;
 import {SimpleStorage} from "./SimpleStorage.sol";
 
 contract StorageFactory {
-    address[] public simpleStorageArray;
-    SimpleStorage simpleStorage;
+    SimpleStorage[] public simpleStorageArray;
     function deploy() public {
-        simpleStorage = new SimpleStorage();
-        simpleStorageArray.push(address(simpleStorage));
+        SimpleStorage simpleStorage = new SimpleStorage();
+        simpleStorageArray.push(simpleStorage);
     }
-
+    function Sf_set (uint256 _index, uint256 _favoriteNumber, string memory _name) public {
+        simpleStorageArray[_index].set(_favoriteNumber, _name);
+    }
+    function Sf_get (uint256 _index, string memory _name) public view returns (uint256) {
+        return simpleStorageArray[_index].get(_name);
+    }
 }
