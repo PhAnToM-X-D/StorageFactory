695b96d036dd3e235a606e7410b2ac0bb231512f
Aradhya Agrawal <aradhya.gzb08@gmail.com>
2026-02-25 01:32:37 +0530

    The storagefactory now successfully deploys the simplestorage contract as many times as wanted

--- StorageFactory.sol
+++ StorageFactory.sol
@@ -4,5 +4,11 @@ pragma solidity ^0.8.0;
 import {SimpleStorage} from "./SimpleStorage.sol";
 
 contract StorageFactory {
-    
+    address[] public simpleStorageArray;
+    SimpleStorage simpleStorage;
+    function deploy() public {
+        simpleStorage = new SimpleStorage();
+        simpleStorageArray.push(address(simpleStorage));
+    }
+
 }
